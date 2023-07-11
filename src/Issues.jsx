882fc4710ceca77f0e98e0a7f4025bfb0086fde4
jiro.octavo <jiro.octavo@gmail.com>
2023-07-11 14:57:14 +0800

    chore: configure details, issues, and markdown

--- src/Issues.jsx
+++ src/Issues.jsx
@@ -1,5 +1,6 @@
 import './reset.css';
 import './App.css';
+import './github-markdown.css';
 import { Link } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import IconOpen from './IconOpen';
@@ -51,7 +52,7 @@ function Issues() {
       {isSuccess && (
         <div className="issues-container">
           <div className="issues-heading">
-            <a href="#">facebook / create-react-app</a>
+            <a href="https://github.com/facebook/create-react-app">facebook / create-react-app</a>
 
             <div className="open-closed-buttons">
               <button onClick={() => setFilter('open')}>
@@ -81,7 +82,7 @@ function Issues() {
                   {issue.state === 'closed' && <IconClosed />}
 
                   <div className="issues-title">
-                    <Link to={`/issues/1`}>{issue.title}</Link>
+                    <Link to={`/issues/${issue.number}`}>{issue.title}</Link>
 
                     <div className="issues-title-details">
                       #{issue.number} opened {
@@ -94,7 +95,7 @@ function Issues() {
                 </div>
 
                 {issue.comments > 0 && (
-                  <Link to={`/issues/1`} className="comments-count-container">
+                  <Link to={`/issues/${issue.number}`} className="comments-count-container">
                     <svg
                       className="octicon octicon-comment v-align-middle"
                       viewBox="0 0 16 16"
@@ -108,7 +109,7 @@ function Issues() {
                         d="M2.75 2.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h2a.75.75 0 01.75.75v2.19l2.72-2.72a.75.75 0 01.53-.22h4.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25H2.75zM1 2.75C1 1.784 1.784 1 2.75 1h10.5c.966 0 1.75.784 1.75 1.75v7.5A1.75 1.75 0 0113.25 12H9.06l-2.573 2.573A1.457 1.457 0 014 13.543V12H2.75A1.75 1.75 0 011 10.25v-7.5z"
                       ></path>
                     </svg>
-                    <div class="comments-count">{issue.comments}</div>
+                    <div className="comments-count">{issue.comments}</div>
                   </Link>
                 )}
               </div>
